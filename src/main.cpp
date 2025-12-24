3ae0560f5ca56d1bea4874ca5b43fc23ac4b1cda
Sergey <AlterForCoding@yandex.ru>
2025-12-25 00:25:04 +0300

    Prototype

--- src/main.cpp
+++ src/main.cpp
@@ -3,55 +3,95 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sstream>
+#include <algorithm>
 
-std::vector<std::string> split(const std::string &str, char d)
+using ip_type = std::vector<std::string>;
+
+void print_ip(const std::vector<ip_type> ip_pool) {
+    for(const auto& ip: ip_pool) {
+        std::string addres;
+        for(const auto& ip_part: ip) {
+            if(!addres.empty()) addres += '.';
+            addres += ip_part;
+        }
+        std::cout << addres << '\n';
+    }
+}
+
+ip_type split(const std::string &str, char separator)
 {
-    std::vector<std::string> r;
+    ip_type result_ip;
+    std::stringstream stream(str);
+    std::string token;
 
-    std::string::size_type start = 0;
-    std::string::size_type stop = str.find_first_of(d);
-    while(stop != std::string::npos)
-    {
-        r.push_back(str.substr(start, stop - start));
-
-        start = stop + 1;
-        stop = str.find_first_of(d, start);
+    // Более читабельно и для используемых данных не большая раздница в производительности
+    while(std::getline(stream, token, separator)) {
+        result_ip.push_back(token);
     }
 
-    r.push_back(str.substr(start));
+    return result_ip;
+}
 
-    return r;
+// Сортировка в обратном лексикографическом порядке
+void revers_sort(std::vector<ip_type>& ip_pool) {
+    std::sort(ip_pool.begin(), ip_pool.end(), [](const ip_type& first, const ip_type& second){
+        for(int item_index = 0; item_index < first.size(); ++item_index)
+        {
+            int f_item = std::stoi(first[item_index]);
+            int s_item = std::stoi(second[item_index]);
+
+            if(f_item != s_item) return f_item > s_item;
+        }
+        return false;
+    });
+    print_ip(ip_pool);
+}
+
+// Первый байт 1
+void sort_first_one(std::vector<ip_type>& ip_pool) {
+    std::vector<ip_type> first_one;
+    for(const auto& ip: ip_pool) {
+        if (std::stoi(ip[0]) == 1) first_one.emplace_back(ip);
+    }
+    print_ip(first_one);
+}
+
+// Первый байт 46, второй 70
+void sort_first_fs_second_st(std::vector<ip_type>& ip_pool) {
+    std::vector<ip_type> first_one;
+    for(const auto& ip: ip_pool) {
+        if (std::stoi(ip[0]) == 46 && std::stoi(ip[1]) == 70) first_one.emplace_back(ip);
+    }
+    print_ip(first_one);
+}
+
+// Любой байт 46
+void sort_any_fs(std::vector<ip_type>& ip_pool) {
+std::vector<ip_type> first_one;
+    for(const auto& ip: ip_pool) {
+       for(const auto& ip_item: ip) {
+        if(std::stoi(ip_item) == 46) first_one.emplace_back(ip);
+       }        
+    }
+    print_ip(first_one);
 }
 
 int main(int argc, char const *argv[])
 {
-    try
-    {
-        std::vector<std::vector<std::string> > ip_pool;
+    try {
+        std::vector<ip_type> ip_pool;
 
-        for(std::string line; std::getline(std::cin, line);)
-        {
-            std::vector<std::string> v = split(line, '\t');
-            ip_pool.push_back(split(v.at(0), '.'));
+        for(std::string line; std::getline(std::cin, line);) {
+            ip_pool.push_back(split(split(line, '\t').at(0), '.'));
         }
 
-        for(std::vector<std::vector<std::string> >::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
-        {
-            for(std::vector<std::string>::const_iterator ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part)
-            {
-                if (ip_part != ip->cbegin())
-                {
-                    std::cout << ".";
-
-                }
-                std::cout << *ip_part;
-            }
-            std::cout << std::endl;
-        }
-
+        revers_sort(ip_pool);
+        sort_first_one(ip_pool);
+        sort_first_fs_second_st(ip_pool);
+        sort_any_fs(ip_pool);
     }
-    catch(const std::exception &e)
-    {
+    catch(const std::exception &e) {
         std::cerr << e.what() << std::endl;
     }
 
